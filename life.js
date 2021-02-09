3214d6a380e95a77dacd71c4152bb2e1e047256d
VsevolodKozlov-git <71010414+VsevolodKozlov-git@users.noreply.github.com>
2021-02-09 15:45:38 +0300

    init

---
+++ life.js
@@ -0,0 +1,13 @@
+const cols = 20; // width
+const rows = 20; // height
+
+
+function createGrid(){
+  resGrid = []
+  for (let row = 0; row < rows.length; row++) {
+    for (let col = 0; col < cols.length; col++) {
+      
+
+    }
+  }
+}
